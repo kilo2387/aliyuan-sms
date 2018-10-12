ee74da2ee1ab388870afec9b0f39cfbc40c44abd
stan <stan87@foxmail.com>
2018-10-13 03:06:59 +0800

    test

---
+++ lib/Test.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: stan
+ * Date: 2018/10/13
+ * Time: 上午1:29
+ */
+
+namespace stan\aliyunsms\lib;
+
+class Test{
+    public static function outPut(){
+        echo 'something';
+    }
+}
